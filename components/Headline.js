f042950a4b49b6f129ca95844953dc5dfd442773
kenpi13 <kenkenpi0526@gmail.com>
2022-03-12 20:51:06 +0900

    ♻️ コンポーネントにまとめる

--- components/Headline.js
+++ components/Headline.js
@@ -2,16 +2,16 @@ import styles from '../styles/Home.module.css'
 import React from 'react'
 
 export function Headline(props) {
-  console.log(props);
   return (
     <div>
-        <h1 className={styles.title}>
-          {props.page} page
-        </h1>
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/{props.page}.js</code>
-        </p>
+      <h1 className={styles.title}>
+        {props.page} page
+      </h1>
+      <p className={styles.description}>
+        Get started by editing{' '}
+        {props.children}
+      </p>
+      <button onClick={props.onClick}>ボタン</button>
     </div>
   )
 }
